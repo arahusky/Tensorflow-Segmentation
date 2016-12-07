89d4116c6fbc4003ceecb7589fbdc033386928ec
Jakub Naplava <arahusky@seznam.cz>
2016-12-07 21:27:06 +0100

    Convolutional autoencoder implementation, 28x28 images

---
+++ libs/utils.py
@@ -0,0 +1,117 @@
+"""Some useful utilities when dealing with neural nets w/ tensorflow.
+
+Parag K. Mital, Jan. 2016
+"""
+import tensorflow as tf
+import numpy as np
+
+
+def montage_batch(images):
+    """Draws all filters (n_input * n_output filters) as a
+    montage image separated by 1 pixel borders.
+
+    Parameters
+    ----------
+    batch : numpy.ndarray
+        Input array to create montage of.
+
+    Returns
+    -------
+    m : numpy.ndarray
+        Montage image.
+    """
+    img_h = images.shape[1]
+    img_w = images.shape[2]
+    n_plots = int(np.ceil(np.sqrt(images.shape[0])))
+    m = np.ones(
+        (images.shape[1] * n_plots + n_plots + 1,
+         images.shape[2] * n_plots + n_plots + 1, 3)) * 0.5
+
+    for i in range(n_plots):
+        for j in range(n_plots):
+            this_filter = i * n_plots + j
+            if this_filter < images.shape[0]:
+                this_img = images[this_filter, ...]
+                m[1 + i + i * img_h:1 + i + (i + 1) * img_h,
+                  1 + j + j * img_w:1 + j + (j + 1) * img_w, :] = this_img
+    return m
+
+
+# %%
+def montage(W):
+    """Draws all filters (n_input * n_output filters) as a
+    montage image separated by 1 pixel borders.
+
+    Parameters
+    ----------
+    W : numpy.ndarray
+        Input array to create montage of.
+
+    Returns
+    -------
+    m : numpy.ndarray
+        Montage image.
+    """
+    W = np.reshape(W, [W.shape[0], W.shape[1], 1, W.shape[2] * W.shape[3]])
+    n_plots = int(np.ceil(np.sqrt(W.shape[-1])))
+    m = np.ones(
+        (W.shape[0] * n_plots + n_plots + 1,
+         W.shape[1] * n_plots + n_plots + 1)) * 0.5
+    for i in range(n_plots):
+        for j in range(n_plots):
+            this_filter = i * n_plots + j
+            if this_filter < W.shape[-1]:
+                m[1 + i + i * W.shape[0]:1 + i + (i + 1) * W.shape[0],
+                  1 + j + j * W.shape[1]:1 + j + (j + 1) * W.shape[1]] = (
+                    np.squeeze(W[:, :, :, this_filter]))
+    return m
+
+
+
+
+# %%
+def corrupt(x):
+    """Take an input tensor and add uniform masking.
+
+    Parameters
+    ----------
+    x : Tensor/Placeholder
+        Input to corrupt.
+
+    Returns
+    -------
+    x_corrupted : Tensor
+        50 pct of values corrupted.
+    """
+    return tf.mul(x, tf.cast(tf.random_uniform(shape=tf.shape(x),
+                                               minval=0,
+                                               maxval=2,
+                                               dtype=tf.int32), tf.float32))
+
+
+# %%
+def weight_variable(shape):
+    '''Helper function to create a weight variable initialized with
+    a normal distribution
+
+    Parameters
+    ----------
+    shape : list
+        Size of weight variable
+    '''
+    initial = tf.random_normal(shape, mean=0.0, stddev=0.01)
+    return tf.Variable(initial)
+
+
+# %%
+def bias_variable(shape):
+    '''Helper function to create a bias variable initialized with
+    a constant value.
+
+    Parameters
+    ----------
+    shape : list
+        Size of weight variable
+    '''
+    initial = tf.random_normal(shape, mean=0.0, stddev=0.01)
+    return tf.Variable(initial)
