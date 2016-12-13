f12163a0dd376eddeb09ddc79c15f71e6d152570
Jan Klůj <jan.kluj@gmail.com>
2016-12-13 18:02:31 +0100

    Plot only few images + gitignore updated

--- convolutional_autoencoder.py
+++ convolutional_autoencoder.py
@@ -351,9 +351,10 @@ def train():
                         network.inputs: np.reshape(test_inputs,
                                                    [n_examples, network.IMAGE_HEIGHT, network.IMAGE_WIDTH, 1])})
 
-                    fig, axs = plt.subplots(4, n_examples, figsize=(n_examples * 3, 10))
+                    n_examples_to_plot = 12
+                    fig, axs = plt.subplots(4, n_examples_to_plot, figsize=(n_examples_to_plot * 3, 10))
                     fig.suptitle("Accuracy: {}, {}".format(test_accuracy, network.description), fontsize=20)
-                    for example_i in range(n_examples):
+                    for example_i in range(n_examples_to_plot):
                         axs[0][example_i].imshow(test_inputs[example_i], cmap='gray')
                         axs[1][example_i].imshow(test_targets[example_i].astype(np.float32), cmap='gray')
                         axs[2][example_i].imshow(
